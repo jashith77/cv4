ac53a1b1b55efd382e2f77961f013ef4b9df9cab
jashith77 <59876671+jashith77@users.noreply.github.com>
2020-04-03 01:49:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ randomNumber1 = randomNumber1*6;
 randomNumber1 = Math.floor(randomNumber1);
 randomNumber1 = randomNumber1+1;
 
-var randomimage = "images/dice"+randomNumber1+".png";
+var randomimage = "https://raw.githubusercontent.com/jashith77/cv4/master/dice"+randomNumber1+".png";
 document.getElementsByTagName("img")[0].setAttribute("src", randomimage);
 
 var randomNumber2 = Math.random();
@@ -11,8 +11,8 @@ randomNumber2 = randomNumber2*6;
 randomNumber2 = Math.floor(randomNumber2);
 randomNumber2 = randomNumber2+1;
 
-var randomimage = "images/dice"+randomNumber2+".png";
-document.getElementsByTagName("img")[1].setAttribute("src", randomimage)
+var randomimage2 = "https://raw.githubusercontent.com/jashith77/cv4/master/dice"+randomNumber2+".png";
+document.getElementsByTagName("img")[1].setAttribute("src", randomimage2)
 
 if(randomNumber1>randomNumber2){
   document.querySelector("h1").innerHTML = "Player 1 is the Winner";
